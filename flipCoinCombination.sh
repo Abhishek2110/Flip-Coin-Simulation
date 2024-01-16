ec0451ab66045f8742aa9e03c76c4780397068a2
Abhishek Rajpal <86553778+Abhishek2110@users.noreply.github.com>
2024-01-16 16:25:08 +0530

    Doublet Combination Percentage Feature Added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -31,9 +31,64 @@ for key in "${!myDictionary[@]}"; do
 	fi 
 done
 length=${#myDictionary[@]}
-percentage=$(echo "scale=2; ($h / $length) * 100" | bc )
-percentage2=$(echo "scale=2; ($t / $length) * 100" | bc)
+percentage=$(( (h * 100) / $length ))
+percentage2=$(( (t * 100) / $length ))
 
 # Print the result
 echo "The percentage of Singlet Heads is: $percentage%"
 echo "The percentage of Singlet Tails is: $percentage2%"
+
+#Function to simulate doublet combination
+declare -A Doublet
+flip_coin2(){
+	for ((i=0; i<=10; i++)); do
+		firsttoss=$((RANDOM%2))
+		secondtoss=$((RANDOM%2))
+		if [[ $firsttoss -eq 1 && secondtoss -eq 1 ]]; then
+			result="HH"
+		elif [[ $firsttoss -eq 1 && secondtoss -eq 0 ]]; then
+			result="HT"
+		elif [[ $firsttoss -eq 0 && secondtoss -eq 1 ]]; then
+                        result="TH"
+		else
+                        result="TT"
+		fi
+		Doublet["$i"]=$result
+	done
+}
+flip_coin2
+
+#Displaying the Dictionary Elements
+echo "Dictionary Elements:"
+for key in "${!Doublet[@]}"; do
+    echo "$key: ${Doublet[$key]}"
+done
+
+#Finding the percentage for Doublet Combination
+hh=0
+ht=0
+th=0
+tt=0
+for key in "${!Doublet[@]}"; do
+        if [ "${Doublet[$key]}" == "HH" ]; then
+            ((hh++))
+        elif [ "${Doublet[$key]}" == "HT" ]; then
+            ((ht++))
+	elif [ "${Doublet[$key]}" == "TH" ]; then
+            ((th++))
+	else
+	    ((tt++))
+        fi
+done
+length1=${#Doublet[@]}
+percentage_hh=$(( (hh * 100) / $length1 ))
+percentage_ht=$(( (ht * 100) / $length1 ))
+percentage_th=$(( (th * 100) / $length1 ))
+percentage_tt=$(( (tt * 100) / $length1 ))
+
+#Print the result
+echo "The percentage of Doublet HH is: $percentage_hh%"
+echo "The percentage of Doublet HT is: $percentage_ht%"
+echo "The percentage of Doublet TH is: $percentage_th%"
+echo "The percentage of Doublet TT is: $percentage_tt%"
+
